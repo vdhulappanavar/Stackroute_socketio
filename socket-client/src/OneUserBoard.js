911bb7b68dad3d0f37b3d93dcb1d5aeacd273681
vdhulappanavar <vdhulappanavar@Vageeshs-MacBook-Air.local>
2018-03-13 14:19:22 +0530

    removed node modules

--- socket-client/src/OneUserBoard.js
+++ socket-client/src/OneUserBoard.js
@@ -27,49 +27,24 @@ function Square(props) {
   
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
 export class OneUserBoard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
       xIsNext: true,
+      setShipLocSate: false
     };
+  }
+
+  changeSetShipLocSate = ()=>{
+    this.setState({
+      setShipLocSate : true
+    })
   }
 
   handleClick(i, j) {
     this.props.changeHitMissArray(i, j)
-    // if(this.props.shipLocs[i][j] ==1){
-    //   console.log("ship there")
-    // }
-    // const squares = this.state.squares.slice();
-    // if (calculateWinner(squares) || squares[i]) {
-    //   return;
-    // }
-    // squares[i] = this.state.xIsNext ? 'X' : 'O';
-    // this.setState({
-    //   squares: squares,
-    //   xIsNext: !this.state.xIsNext,
-    // });
   }
 
   renderSquare(i, j) {
@@ -81,16 +56,20 @@ export class OneUserBoard extends React.Component {
     );
   }
 
+  setShipLoc = (i, j)=>{
+    console.log("in set ship loc")
+  }
+
   render() {
     //const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if (winner) {
-        status = 'Winner: ' + winner;
-      } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
+      // const winner = calculateWinner(this.state.squares);
+      // let status;
+      // if (winner) {
+      //   status = 'Winner: ' + winner;
+      // } else {
+      //   status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      // }
   
     // return (
     //   <div>
@@ -121,130 +100,139 @@ export class OneUserBoard extends React.Component {
     //   })
     //   list.push(</div>)
     // })
+
+    let shipLocGrid=''
+    if(this.state.setShipLocSate){
+      shipLocGrid=`HEY`
+    }
     return(
       <div>
-        {/* {list}       */}
-        <div className="board-row">
-          {this.renderSquare(0, 0)}
-          {this.renderSquare(0, 1)}
-          {this.renderSquare(0, 2)}
-          {this.renderSquare(0, 3)}
-          {this.renderSquare(0, 4)}
-          {this.renderSquare(0, 5)}
-          {this.renderSquare(0, 6)}
-          {this.renderSquare(0, 7)}
-          {this.renderSquare(0, 8)}
-          {this.renderSquare(0, 9)}
+        <div>
+          {/* {list}       */}
+          <div className="board-row">
+            {this.renderSquare(0, 0)}
+            {this.renderSquare(0, 1)}
+            {this.renderSquare(0, 2)}
+            {this.renderSquare(0, 3)}
+            {this.renderSquare(0, 4)}
+            {this.renderSquare(0, 5)}
+            {this.renderSquare(0, 6)}
+            {this.renderSquare(0, 7)}
+            {this.renderSquare(0, 8)}
+            {this.renderSquare(0, 9)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(1, 0)}
+            {this.renderSquare(1, 1)}
+            {this.renderSquare(1, 2)}
+            {this.renderSquare(1, 3)}
+            {this.renderSquare(1, 4)}
+            {this.renderSquare(1, 5)}
+            {this.renderSquare(1, 6)}
+            {this.renderSquare(1, 7)}
+            {this.renderSquare(1, 8)}
+            {this.renderSquare(1, 9)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(2, 0)}
+            {this.renderSquare(2, 1)}
+            {this.renderSquare(2, 2)}
+            {this.renderSquare(2, 3)}
+            {this.renderSquare(2, 4)}
+            {this.renderSquare(2, 5)}
+            {this.renderSquare(2, 6)}
+            {this.renderSquare(2, 7)}
+            {this.renderSquare(2, 8)}
+            {this.renderSquare(2, 9)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3, 0)}
+            {this.renderSquare(3, 1)}
+            {this.renderSquare(3, 2)}
+            {this.renderSquare(3, 3)}
+            {this.renderSquare(3, 4)}
+            {this.renderSquare(3, 5)}
+            {this.renderSquare(3, 6)}
+            {this.renderSquare(3, 7)}
+            {this.renderSquare(3, 8)}
+            {this.renderSquare(3, 9)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(4, 0)}
+            {this.renderSquare(4, 1)}
+            {this.renderSquare(4, 2)}
+            {this.renderSquare(4, 3)}
+            {this.renderSquare(4, 4)}
+            {this.renderSquare(4, 5)}
+            {this.renderSquare(4, 6)}
+            {this.renderSquare(4, 7)}
+            {this.renderSquare(4, 8)}
+            {this.renderSquare(4, 9)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(5, 0)}
+            {this.renderSquare(5, 1)}
+            {this.renderSquare(5, 2)}
+            {this.renderSquare(5, 3)}
+            {this.renderSquare(5, 4)}
+            {this.renderSquare(5, 5)}
+            {this.renderSquare(5, 6)}
+            {this.renderSquare(5, 7)}
+            {this.renderSquare(5, 8)}
+            {this.renderSquare(5, 9)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6, 0)}
+            {this.renderSquare(6, 1)}
+            {this.renderSquare(6, 2)}
+            {this.renderSquare(6, 3)}
+            {this.renderSquare(6, 4)}
+            {this.renderSquare(6, 5)}
+            {this.renderSquare(6, 6)}
+            {this.renderSquare(6, 7)}
+            {this.renderSquare(6, 8)}
+            {this.renderSquare(6, 9)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(7, 0)}
+            {this.renderSquare(7, 1)}
+            {this.renderSquare(7, 2)}
+            {this.renderSquare(7, 3)}
+            {this.renderSquare(7, 4)}
+            {this.renderSquare(7, 5)}
+            {this.renderSquare(7, 6)}
+            {this.renderSquare(7, 7)}
+            {this.renderSquare(7, 8)}
+            {this.renderSquare(7, 9)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(8, 0)}
+            {this.renderSquare(8, 1)}
+            {this.renderSquare(8, 2)}
+            {this.renderSquare(8, 3)}
+            {this.renderSquare(8, 4)}
+            {this.renderSquare(8, 5)}
+            {this.renderSquare(8, 6)}
+            {this.renderSquare(8, 7)}
+            {this.renderSquare(8, 8)}
+            {this.renderSquare(8, 9)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(9, 0)}
+            {this.renderSquare(9, 1)}
+            {this.renderSquare(9, 2)}
+            {this.renderSquare(9, 3)}
+            {this.renderSquare(9, 4)}
+            {this.renderSquare(9, 5)}
+            {this.renderSquare(9, 6)}
+            {this.renderSquare(9, 7)}
+            {this.renderSquare(9, 8)}
+            {this.renderSquare(9, 9)}
+          </div>
         </div>
-        <div className="board-row">
-          {this.renderSquare(1, 0)}
-          {this.renderSquare(1, 1)}
-          {this.renderSquare(1, 2)}
-          {this.renderSquare(1, 3)}
-          {this.renderSquare(1, 4)}
-          {this.renderSquare(1, 5)}
-          {this.renderSquare(1, 6)}
-          {this.renderSquare(1, 7)}
-          {this.renderSquare(1, 8)}
-          {this.renderSquare(1, 9)}
+        <button onClick = {this.changeSetShipLocSate}> Set My own Positions</button>
+        {shipLocGrid}
         </div>
-        <div className="board-row">
-          {this.renderSquare(2, 0)}
-          {this.renderSquare(2, 1)}
-          {this.renderSquare(2, 2)}
-          {this.renderSquare(2, 3)}
-          {this.renderSquare(2, 4)}
-          {this.renderSquare(2, 5)}
-          {this.renderSquare(2, 6)}
-          {this.renderSquare(2, 7)}
-          {this.renderSquare(2, 8)}
-          {this.renderSquare(2, 9)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3, 0)}
-          {this.renderSquare(3, 1)}
-          {this.renderSquare(3, 2)}
-          {this.renderSquare(3, 3)}
-          {this.renderSquare(3, 4)}
-          {this.renderSquare(3, 5)}
-          {this.renderSquare(3, 6)}
-          {this.renderSquare(3, 7)}
-          {this.renderSquare(3, 8)}
-          {this.renderSquare(3, 9)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(4, 0)}
-          {this.renderSquare(4, 1)}
-          {this.renderSquare(4, 2)}
-          {this.renderSquare(4, 3)}
-          {this.renderSquare(4, 4)}
-          {this.renderSquare(4, 5)}
-          {this.renderSquare(4, 6)}
-          {this.renderSquare(4, 7)}
-          {this.renderSquare(4, 8)}
-          {this.renderSquare(4, 9)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(5, 0)}
-          {this.renderSquare(5, 1)}
-          {this.renderSquare(5, 2)}
-          {this.renderSquare(5, 3)}
-          {this.renderSquare(5, 4)}
-          {this.renderSquare(5, 5)}
-          {this.renderSquare(5, 6)}
-          {this.renderSquare(5, 7)}
-          {this.renderSquare(5, 8)}
-          {this.renderSquare(5, 9)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6, 0)}
-          {this.renderSquare(6, 1)}
-          {this.renderSquare(6, 2)}
-          {this.renderSquare(6, 3)}
-          {this.renderSquare(6, 4)}
-          {this.renderSquare(6, 5)}
-          {this.renderSquare(6, 6)}
-          {this.renderSquare(6, 7)}
-          {this.renderSquare(6, 8)}
-          {this.renderSquare(6, 9)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(7, 0)}
-          {this.renderSquare(7, 1)}
-          {this.renderSquare(7, 2)}
-          {this.renderSquare(7, 3)}
-          {this.renderSquare(7, 4)}
-          {this.renderSquare(7, 5)}
-          {this.renderSquare(7, 6)}
-          {this.renderSquare(7, 7)}
-          {this.renderSquare(7, 8)}
-          {this.renderSquare(7, 9)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(8, 0)}
-          {this.renderSquare(8, 1)}
-          {this.renderSquare(8, 2)}
-          {this.renderSquare(8, 3)}
-          {this.renderSquare(8, 4)}
-          {this.renderSquare(8, 5)}
-          {this.renderSquare(8, 6)}
-          {this.renderSquare(8, 7)}
-          {this.renderSquare(8, 8)}
-          {this.renderSquare(8, 9)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(9, 0)}
-          {this.renderSquare(9, 1)}
-          {this.renderSquare(9, 2)}
-          {this.renderSquare(9, 3)}
-          {this.renderSquare(9, 4)}
-          {this.renderSquare(9, 5)}
-          {this.renderSquare(9, 6)}
-          {this.renderSquare(9, 7)}
-          {this.renderSquare(9, 8)}
-          {this.renderSquare(9, 9)}
-        </div>
-      </div>
     )
   }
 }
